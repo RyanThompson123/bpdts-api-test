85e162ea467df192065487539a815ce77f3dd613
Ryan Thompson <ryan.thompson@engineering.digital.dwp.gov.uk>
2020-10-12 17:53:47 +0100

    build: spring initializr run (#1)
    Run spring initializr to set up base of project

---
+++ src/main/java/com/ryanthompson123/bpdtsapitest/BpdtsApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.ryanthompson123.bpdtsapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpdtsApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpdtsApiTestApplication.class, args);
+	}
+
+}
