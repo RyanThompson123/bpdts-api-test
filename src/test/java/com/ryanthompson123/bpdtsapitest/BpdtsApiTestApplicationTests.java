94c553cae49f2c286268950577717f1a833cd535
Ryan Thompson <ryan.thompson@engineering.digital.dwp.gov.uk>
2020-10-12 17:47:41 +0100

    build: spring initializr run

---
+++ src/test/java/com/ryanthompson123/bpdtsapitest/BpdtsApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryanthompson123.bpdtsapitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpdtsApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
